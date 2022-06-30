913e6cd221efbfbdc79e80deaaf7999ed096f012
ManFromRzeszotary <roman.tonia@gmail.com>
2022-06-30 13:51:41 +0200

    27JUN same directory str and int constans 27JUN same directory single/multi args date, time, add function

---
+++ Flask_basics.py
@@ -0,0 +1,78 @@
+from flask import Flask
+from flask import request
+import datetime
+from datetime import datetime
+import random
+from random import randint
+
+app = Flask(__name__)
+
+
+@app.route("/<string:name>")
+def hello(name):
+    return 'Hello ' + name + '!'
+
+
+@app.route("/<int:age>")  # no parameter methods passed defaults to GET
+def age(age):
+    return 'You are ' + str(age) + ' and still alive!'
+
+@app.route("/date")  # no parameter methods passed defaults to GET
+def display_date():
+    #actual date
+    return 'It is  ' + str(datetime.today().strftime('%Y-%m-%d')) + ' have a nice day'
+
+@app.route("/time")  # no parameter methods passed defaults to GET
+def display_time():
+    #actual time
+    return 'It is  ' + str(datetime.today().strftime('%H:%M')) + ' have a nice day'
+
+@app.route("/licz/<a>/<b>")  # no parameter methods passed defaults to GET
+def sum_nbrs(a,b):
+    answer = float(a) + float(b)
+    return 'adding ' + str(a) + ' to ' + str(b) + ' gives: ' + str(answer)
+
+@app.route("/losuj")  # no parameter methods passed defaults to GET
+def sum_nbrs(a,b):
+    answer = float(a) + float(b)
+    return 'adding ' + str(a) + ' to ' + str(b) + ' gives: ' + str(answer)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # obsłuż dane logowania
+        return 'pass data with POST method'
+    else:
+        return 'get data with GET method'
+
+
+# Dane, które przesyłamy za pomocą formularza metodą GET znajdują się w obiekcie request.args,
+# natomiast te przysyłane metodą POST znajdują się w request.form.
+# Dostać się do nich możemy dzięki konstrukcji request.args["nazwa-klucza"] lub request.form["nazwa-klucza"].
+# Jeśli chcemy odebrać wiele danych o tej samej nazwie, musimy użyć następujacego kodu:
+# request.args.getlist(<nazwa>)
+# lub
+# request.form.getlist(<nazwa>)
+# gdzie nazwa to wartość atrybutu name z HTML-a. Metoda zwraca listę wartości.
+# form.html
+# < input
+# type = "checkbox"
+# name = "pizza_topping"
+# value = "cheese" >
+# dodatkowy
+# ser
+# # < input
+# type = "checkbox"
+# name = "pizza_topping"
+# value = "ham" >
+# szynka
+# # app.py
+# # @app.route("/pizza/add_toppings", method=["POST"])
+# # def add_toppings():
+#     tops = request.form.getlist("pizza_topping")
+# #     # dalej kod
+
+if __name__ == "__main__":
+    app.run(debug=True)
